40f22939c9b5ba4b495a1c657778ecf7598cd858
Melinda Owen <Melinda Owen>
2017-05-03 19:25:40 -0400

    added starter text for our routes, schema, user.js and index.js

---
+++ models/user.js
@@ -0,0 +1,45 @@
+module.exports = function(sequelize, DataTypes) {
+    var User = sequelize.define("User", {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull: false
+            },
+            email: {
+                type: DataTypes.STRING,
+                validate: {
+                    isEmail: true
+                }
+            },
+            username: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            hash: {
+                type: DataTypes.STRING
+            },
+            salt: {
+                type: DataTypes.STRING
+            },
+            role: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                defaultValue: 'user',
+                isIn: [
+                    ['admin', 'user']
+                ]
+            }
+        },
+        // To create an association between Users and Players
+        {
+            classMethods: {
+                associate: function(models) {
+                    // 
+                    User.hasMany(models.Player);
+                }
+            }
+        }
+    );
+    return User;
+};
---
+++ server.js
@@ -0,0 +1,58 @@
+// *****************************************************************************
+// Server.js - Initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var expressValidator = require('express-validator');
+var expressSession = require('express-session');
+
+
+// Set up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Requiring models for syncing
+var db = require("./models");
+
+// Set up Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+//After the body is parsed, it's time for validation
+//this starts the express validator
+app.use(expressValidator());
+
+// Static directory
+app.use(express.static("./public"));
+
+//At the end here we add express session
+//Express Session
+app.use(expressSession({
+	secret: 'secret code',
+	//If saveUnitialized is set to true it will save a session to our session storage even if it is not initialized 
+	saveUninitialized: false,
+	//If resave is set to true it will save our session after each request
+	//false will only save if we change something
+	resave: false
+}));
+
+// Routes =============================================================
+
+// Requiring html-routes.js to route html files
+require("./controllers/html-routes.js")(app);
+require("./controllers/api-routes.js")(app);
+
+// Syncing sequelize models, then starting express app
+db.sequelize.sync({force: false}).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
+
+
+
