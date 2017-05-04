3bbd28f6a80cbc357a0ce43b55690003943f1aa4
Matt Ames <mattames@carolina.rr.com>
2017-05-04 01:39:27 -0400

    functional express/react site showing a blank page.  Dammit.

--- app/components/children/adminMap.js
+++ app/components/children/adminMap.js
@@ -7,22 +7,24 @@ var AdminMap = React.createClass({
     // Here we set a generic state associated with the text being searched for
     getInitialState: function () {
 
-        // TODO  fix me
-        return {term: ""};
+        // TODO
+        // we need to query db and return two arrays
+        // one for users and one for admins
+        // then we need to populate a map object with 2 different
+        // colored pins based on user/admin
+        // return {TODO};
     },
 
 
     // Here we describe this component's render method
     render: function () {
 
-            <div class="row">
-                <div id="street-view">
-                    // insert street view via javascript
-                </div>
-                <div id="map-view">
-                    // <!-- insert street map via html -->
-                </div>
+        <div class="row">
+            <div id="map-view">
+            </div>
         </div>
 
     }
-  },
+});
+// Export the component back for use in other files
+module.exports = AdminMap
