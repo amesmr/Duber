000a6009fcbe96fcea880d2f03d7423bd0bf8176
Matt Ames <mattames@carolina.rr.com>
2017-05-09 22:35:40 -0400

    Further attempts to get routes working in ES6.  Still not working

--- app/app.jsx
+++ app/app.jsx
@@ -2,8 +2,9 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-// Grabs the Routes
+
 import routes from "./routes/routes.jsx"
+// import Main from "./components/Main.jsx"
 
 // This code here allows us to render our main component (in this case Main)
 ReactDOM.render(routes, document.getElementById("app"));
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,63 +1,22 @@
 "use strict";
 // Include React
-import React, { Component, PropTypes } from 'react';
-
+import React, { Component, PropTypes } from 'react'
+import { Link } from 'react-router'
 
 // Reference the high-level components
 // import AdminLogin from "./children/AdminLogin"
 import AdminSignup from "./children/AdminSignup.jsx"
 import AdminMap from "./children/AdminMap.jsx"
 // import Animals from "./children/Animals.jsx"
-// import About from './children/About.jsx'
+import About from './children/About.jsx'
 
 // Helper for making AJAX requests to our API
 import helpers from "./utils/helpers.jsx"
 
-// Creating the Main component
-
   // TODO
   // figure out what needs to be set in this initial state
+
 export default class Main extends React.Component {
-    //     getInitialState: function () {
-    //     // This ain't it
-    //     // return { searchTerm: "", results: "", history: [] };
-    //   },
-
-    //   // The moment the page renders get the History
-    //   componentDidMount: function() {
-
-    //   },
-
-    //   // If the component changes (i.e. if a search is entered)...
-    //   componentDidUpdate: function() {
-
-    //     // Run the query for the address
-    //     helpers.runQuery(this.state.searchTerm).then(function(data) {
-    //       if (data !== this.state.results) {
-    //         console.log("Address", data);
-    //         this.setState({ results: data });
-
-    //         // After we've received the result... then post the search term to our history.
-    //         helpers.postHistory(this.state.searchTerm).then(function() {
-    //           console.log("Updated!");
-
-    //           // After we've done the post... then get the updated history
-    //           helpers.getHistory().then(function(response) {
-    //             console.log("Current History", response.data);
-
-    //             console.log("History", response.data);
-
-    //             this.setState({ history: response.data });
-
-    //           }.bind(this));
-    //         }.bind(this));
-    //       }
-    //     }.bind(this));
-    //   },
-    //   // This function allows childrens to update the parent.
-    //   setTerm: function(term) {
-    //     this.setState({ searchTerm: term });
-    //   },
     // Here we render the function
     render() {
         return (
@@ -69,13 +28,20 @@ export default class Main extends React.Component {
                         </div>
                     </div>
                 </div>
-                <div className="container" id="main-content">
+                <div className="links">
+                    <ul role="nav">
+                        <li><Link to="/adminSignup">About</Link></li>
+                        <li><Link to="/adminMap">AdminMap</Link></li>
+                    </ul>
+
                 </div>
-                <a href="adminSignup"> Admin Signup</a>
+                <div className="container" id="main-content">
+                    {this.props.children}
+                </div>
                 <div className="row">
                     <About />
                 </div>
             </div>
-        );
+        )
     }
 }
